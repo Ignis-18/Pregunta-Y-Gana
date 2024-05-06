d71b78a2d3a6cf748c376253709394d541ee209f
Ignis-18 <lcm_dev@outlook.com>
2024-05-06 13:08:50 -0400

    Implementado boton en blanco y negro

--- Assets/Scripts/Comodin.cs
+++ Assets/Scripts/Comodin.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Comodin : MonoBehaviour
 {
     public Pruebas pruebas;
+    public Sprite ByN;
+    public Sprite color;
+
+    void Update()
+    {
+        if (pruebas.usosComodin<=0)
+        {
+            GetComponent<Image>().sprite = ByN;
+        }
+
+        else
+        {
+            GetComponent<Image>().sprite = color;
+        }
+    }
 
     public void comodin()
     {
